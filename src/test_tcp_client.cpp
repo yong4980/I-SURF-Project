0c9cb9f09de7aad3d0aa7bccdb96b15656ce00fe
yong4980 <yongtae0114@gmail.com>
2019-07-16 15:50:39 -0700

    Create. TCP_client

---
+++ src/test_tcp_client.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+
+#define PORT 4567
+#define PACKET_SIZE 1024
+#define SERVER_IP "169.234.56.234"
+void error_handling(char* message);
+
+int main(int argc, char* argv[]){
+  int sock;
+  struct sockaddr_in server_addr;
+
+  server_sock = socket(PF_INET, SOCK_STREAM, 0);
+  if(server_sock == -1){
+    error_handling("socket() error");
+  }
+
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_addr.s_addr = htonl(SERVER_IP);
+  server_addr.sin_port = htons(PORT);
+
+  if(connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1){
+    error_handling("connect() error");
+  }
+
+  char message[] = "Client Message";
+  char cBuffer[PACKET_SIZE] = {};
+
+  read(client_sock, cBuffer, sizeof(cBuffer));
+  write(client_sock, message, sizeof(message));
+
+  printf("Message from server: %s\n", cBuffer);
+
+  close(sock);
+  return 0;
+}
+
+void error_handling(char* message){
+  fputs(message, stderr);
+  fputc('\n',stderr);
+  exit(1);
+}
