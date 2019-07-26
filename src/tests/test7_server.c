c9aaf1cefcab9110de9d1bc5a897183aec274478
yong4980 <yongtae0114@gmail.com>
2019-07-26 15:22:57 -0700

    Update test source

--- src/tests/test7_server.c
+++ src/tests/test7_server.c
@@ -9,7 +9,31 @@
 
 #define PORT 4444
 
+void test(char* buffer, struct sockaddr_in newAddr, int newSocket){
+  int i;
+  if(strcmp(buffer, "testing1") == 0){
+    send(newSocket, buffer, strlen(buffer), 0);
+    for(i=0; i<10; i++){
+      printf("Client(%d) complete1\n", ntohs(newAddr.sin_port));
+      sleep(1);
+    }
+  }
+  else if(strcmp(buffer, "testing2") == 0){
+    send(newSocket, buffer, strlen(buffer), 0);
+    for(i=0; i<10; i++){
+      printf("Clinet(%d) complete2\n", ntohs(newAddr.sin_port));
+      sleep(1);
+    }
+  }
+  else{
+    send(newSocket, buffer, strlen(buffer), 0);
+  }
+}
+
 int main(){
+
+  //testing
+  int boardNum = 0;
 
   int sockfd, ret;
   struct sockaddr_in serverAddr;
@@ -21,6 +45,8 @@ int main(){
 
   char buffer[1024];
   pid_t childpid;
+
+  bzero(buffer, sizeof(buffer));
 
   sockfd = socket(AF_INET, SOCK_STREAM, 0);
   if(sockfd < 0){
@@ -65,8 +91,9 @@ int main(){
           break;
         }
         else{
-          printf("Client: %s\n", buffer);
-          send(newSocket, buffer, strlen(buffer), 0);
+          printf("Client(%d): %s\n", ntohs(newAddr.sin_port), buffer);
+          //send(newSocket, buffer, strlen(buffer), 0);
+          test(buffer, newAddr, newSocket);
           bzero(buffer, sizeof(buffer));
         }
       }
