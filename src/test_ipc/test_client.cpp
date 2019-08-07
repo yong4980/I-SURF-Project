3cfcde2f52559353840996ded6c621aba65d296e
yong4980 <yongtae0114@gmail.com>
2019-08-07 08:54:47 -0700

    Create prototype IPC

---
+++ src/test_ipc/test_client.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+#include <stdlib.h>  // for strol
+#include <stdio.h>
+#include <string.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <cstdlib>
+#include <fcntl.h>
+#include <semaphore.h>
+//network
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+using namespace std;
+
+#define PORT 4444
+#define HOSTIP "192.168.55.1"
+#define SEM_REQUEST_SIGNAL_NAME "/sem-req"
+#define SEM_ACK_SIGNAL_NAME "/sem-ack"
+
+// specifies the shared memory structure
+struct fuzzy_data
+{
+	// Controller inputs
+	double ipc;
+	double powErr;
+
+	// Contoller outputs
+	int active_cores;
+	int freq;
+
+	//Communication flags for controller and the daemon
+	bool request;
+
+};
+
+void error (string msg)
+{
+
+}
+
+int main(int argc, char* argv[])
+{
+	//network 
+	int clientSocket, ret;
+	struct sockaddr_in serverAddr;
+	char buffer[1024];
+
+	clientSocket = socket(AF_INET, SOCK_STREAM, 0);
+	if(clientSocket < 0){
+		printf("[-]Error in Connection.\n");
+		exit(1);
+	}
+	printf("[+]Client Socket is created.\n");
+
+	memset(&serverAddr, '\0', sizeof(serverAddr));
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(PORT);
+	serverAddr.sin_addr.s_addr = inet_addr(HOSTIP);
+
+	ret = connect(clientSocket, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+
+	if(ret < 0){
+		printf("[-]Error in connection.\n");
+		exit(1);
+	}
+	printf("[+]Connected to Server.\n");
+
+	//ipc 
+	key_t key = ftok("shmfile", 65); // ftok to generate unique key
+	
+	int shmid = shmget(key, 1024, 0666|IPC_CREAT); // shmget returns an identifier in shmid
+
+	fuzzy_data *tmp = (fuzzy_data*) shmat(shmid, (void*)0, 0); // shmat to attach to shared memory
+	sem_t *req_sem, *ack_sem;
+	if((req_sem = sem_open(SEM_REQUEST_SIGNAL_NAME, O_CREAT, 0600, 0)) == SEM_FAILED)
+		error ("sem_open");
+	if((ack_sem = sem_open(SEM_ACK_SIGNAL_NAME, O_CREAT, 0600, 0)) == SEM_FAILED)
+		error ("sem_open");
+
+	//int count = atoi(argv[1]);
+
+/*	//ipc
+	while(1) {
+		//wait for a request
+		while(tmp->request == false);
+		
+		//powerErr = tmp->powErr; //set test_mars
+		//int frequencyVal = fuzzyDVFS(powerErr); //do not use fuzzy
+		//tmp->freq = frequncyVal; //set specific value 
+
+		//Send result to daemon 
+		if (sem_post (ack_sem) == -1)
+			error ("sem_post : buffer_count_sen");
+	}
+	*/
+
+	//interact server
+	while(1){
+		bzero(buffer, sizeof(buffer));
+		printf("Client : \t");
+		scanf("%s", &buffer[0]);
+		send(clientSocket, buffer, strlen(buffer), 0);
+
+		if(strcmp(buffer, ":exit") == 0){
+			close(clientSocket);
+			printf("[-]Disconnected from server.\n");
+			exit(1);
+		}
+
+		if(recv(clientSocket, buffer, 1024, 0) <0){
+			printf("[-]Error in receiving data.\n");
+			exit(1);
+		}
+		/*testing*/	
+		/*else if(strcmp(buffer, "checking") == 0){
+			// ipc
+			while(tmp->request == false);
+			char powerErr[10];
+			sprintf(powerErr, "%f", tmp->powErr);
+			bzero(buffer, sizeof(buffer));
+			strcpy(buffer, "powerErr : ");
+			strcat(buffer, powerErr);
+			tmp->freq = tmp->powErr + 10;
+			if(sem_post(ack_sem) == -1)
+				error("sem_post : buffer_count_sen");
+			// end ipc
+			send(clientSocket, buffer, strlen(buffer), 0);
+		}*/
+		else{
+			printf("server: \t%s\n", buffer);
+		}
+	}
+
+	
+	shmdt(tmp); // detach from shared memory
+	return 0;
+}
---
+++ src/test_ipc/test_server.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#define PORT 4444
+#define HOSTIP "192.168.55.1"
+
+void test(char* buffer, struct sockaddr_in newAddr, int newSocket){
+  int i;
+  if(strcmp(buffer, "testing1") == 0){
+    send(newSocket, buffer, strlen(buffer), 0);
+    for(i=0; i<10; i++){
+      printf("Client(%d) complete1\n", ntohs(newAddr.sin_port));
+      sleep(1);
+    }
+  }
+  else if(strcmp(buffer, "testing2") == 0){
+    send(newSocket, buffer, strlen(buffer), 0);
+    for(i=0; i<10; i++){
+      printf("Clinet(%d) complete2\n", ntohs(newAddr.sin_port));
+      sleep(1);
+    }
+  }
+  else{
+    send(newSocket, buffer, strlen(buffer), 0);
+  }
+}
+
+int main(){
+
+  //testing
+  int boardNum = 0;
+
+  int sockfd, ret;
+  struct sockaddr_in serverAddr;
+
+  int newSocket;
+  struct sockaddr_in newAddr;
+
+  socklen_t addr_size;
+
+  char buffer[1024];
+  pid_t childpid;
+
+  bzero(buffer, sizeof(buffer));
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(sockfd < 0){
+    printf("[-]Error in connection.\n");
+    exit(1);
+  }
+  printf("[+]Server Socket is created.\n");
+
+  memset(&serverAddr, '\0', sizeof(serverAddr));
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(PORT);
+  serverAddr.sin_addr.s_addr = inet_addr(HOSTIP);
+
+  ret = bind(sockfd, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+  if(ret < 0){
+    printf("[-]Error in binding.\n");
+    exit(1);
+  }
+  printf("[+]Bind to port %d\n", PORT);
+
+  if(listen(sockfd, 10)==0){
+    printf("Listening....\n");
+  }
+  else{
+    printf("[-]Error in binding.\n");
+  }
+
+  while(1){
+    newSocket = accept(sockfd, (struct sockaddr*)&newAddr, &addr_size);
+    if(newSocket < 0){
+      exit(1);
+    }
+    printf("[+]Connection accepted from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
+
+    if(childpid = fork() == 0){
+      close(sockfd);
+
+      while(1){
+        recv(newSocket, buffer, 1024, 0);
+        if(strcmp(buffer, ":exit") == 0){
+          printf("[-]Disconnected from %s:%d\n", inet_ntoa(newAddr.sin_addr), ntohs(newAddr.sin_port));
+          break;
+        }
+        else{
+          printf("Client(%d): %s\n", ntohs(newAddr.sin_port), buffer);
+          //send(newSocket, buffer, strlen(buffer), 0);
+          test(buffer, newAddr, newSocket);
+          bzero(buffer, sizeof(buffer));
+        }
+      }
+    }
+  }
+
+  close(newSocket);
+
+  return 0;
+}
